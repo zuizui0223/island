38ee50774547131ddb4c0fdc908d13a47e22f9e9
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-18 15:22:18 +0900

    Allow sub-1e-8 numerical variation in v14 reproduction gate

--- src/island_v2/chapter1_v14_submission_promotion.py
+++ src/island_v2/chapter1_v14_submission_promotion.py
@@ -11,7 +11,7 @@ from typing import Any
 
 RESULT_CONTRACT = "chapter1_v14_reordered_hypotheses_result_v1"
 PROMOTED_CONTRACT = "chapter1_v14_canonical_result_lock_v1"
-TOL = 1e-9
+TOL = 1e-8
 
 
 def _load(path: Path) -> dict[str, Any]:
